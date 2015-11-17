c1e806a63ec864a89f83974620ed6d1d4e2d4e8b
David James Carter <fresco.raja@gmail.com>
2015-11-17 01:59:27 -0800

    added most content to works page

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,9 @@ var $toggle = $("#toggle");
 var $menu = $("#menu");
 var $loadingScreen = $("#intro");
 
-initBrowser();
-initVideo();
+initBrowsers();
+initVideos();
+
 $(window).load(function () {
   setTimeout(function () {
     $loadingScreen.fadeOut(3000,function() {
@@ -20,74 +21,44 @@ $menuToggles.click(function (){
 });
 
 
-function initBrowser() {
-
-  $(".browser_img").each(function() {
-    var $this = $(this);
-    $(this).click(function() {
-      $(this).closest(".browser").toggleClass('maxied');
-    });
-    var title = $this.attr('title');
-    var style = $this.attr('style');
-    if(typeof title === 'undefined') {
-      title = "";
-    }
-    var before_html = '<div class="browser" style="'+style+'">';
-        before_html +=  '<h4 class="title">';
-        before_html +=    title;
-        before_html +=  '</h4>';
-        before_html +=  '<ul class="ui">';
-        before_html +=    '<li class="close"></li>';
-        before_html +=    '<li class="min"></li>';
-        before_html +=    '<li class="max"></li>';
-        before_html +=  '</ul>';
-        before_html +=  '<div class="window" style="'+style+'"></div></div>';
-
-    var after_html = "</div></div>";
-
-    $(this).before(before_html);
-    $(this).appendTo($(this).prev('.browser').find('.window'));
-  });
-
+function initBrowsers() {
   $('.ui .min').click(function(){
-    $(this).closest('.browser').removeClass('maxied').toggleClass('minied');
+    $(this).closest('.browser').removeClass('maxed').toggleClass('minied');
   });
 
   $('.ui .max').click(function(){
-    $(this).closest('.browser').removeClass('minied').toggleClass('maxied');
+    $(this).closest('.browser').removeClass('minied').toggleClass('maxed');
   });
 
   $('.ui .close').click(function() {
-    if(confirm("Are you sure you want to close this project?\n\nYou'll have to refresh the page to see it again, in addition to hurting my feelings.")) {
-      $(this).closest('.browser').removeClass('minied').removeClass('maxied').addClass('closed');
+    if(confirm("Are you sure you want to remove this project from view? \nYou will have to refresh the page to see it again..")) {
+      $(this).closest('.browser').removeClass('minied').removeClass('maxed').addClass('closed');
     }
   });
 }
 
-function initVideo() {
+function initVideos() {
 
   $("video").each(function(){
     var $this = $(this);
     var video = $this.get(0);
     $this.bind('ended', function() {
-      $this.closest(".browser").removeClass('maxied');
+      $this.closest(".browser").removeClass('maxed');
       $this.prev('.play').removeClass('playing');
       video.currentTime = 0;
     });
   });
-
-  $("video").before("<span class='play'></span>");
 
   $(".play").click(function() {
     var $this = $(this);
     $this.toggleClass('playing');
     var video = $(this).next("video").get(0);
     if(video.paused === true) {
-      $(this).closest(".browser").addClass('maxied');
+      $(this).closest(".browser").addClass('maxed');
       video.play();
     } else {
       video.pause();
-      $(this).closest(".browser").removeClass('maxied');
+      $(this).closest(".browser").removeClass('maxed');
     }
   });
 }
