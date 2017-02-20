5b6e0ea8f64b6a11d2396c80c3c7a4d37f28c6d5
David Carter <dcarter@freewheel.tv>
2017-02-20 11:39:54 -0500

    minify js and css, add gulp task for js

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+// script path
+var jsFiles = 'js/*.js',
+    jsDest = 'scripts';
+
+gulp.task('scripts', function() {
+  return gulp.src(jsFiles)
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest(jsDest))
+    .pipe(rename('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(jsDest));
+});
